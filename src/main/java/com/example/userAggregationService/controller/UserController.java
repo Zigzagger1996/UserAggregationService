a121f7e3304db85bfd9883ed53048c51e4c39bdb
Zigzagger1996 <vova.glukhomanyk@gmail.com>
2024-10-25 15:30:51 +0300

    Add OpenAPI support

--- src/main/java/com/example/userAggregationService/controller/UserController.java
+++ src/main/java/com/example/userAggregationService/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.userAggregationService.controller;
 
 import com.example.userAggregationService.model.User;
 import com.example.userAggregationService.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
+    @Operation(summary = "Retrieve a list of users from all DBs added in application.yml")
     public List<User> getUsers() {
         return userService.getAllUsersService();
     }
